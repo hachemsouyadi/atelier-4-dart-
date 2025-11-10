2974644f5058ca5bb6270eaff1249a3787b7e919
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-09 20:09:32 -0500

    creation methode

--- exercice_1.dart
+++ exercice_1.dart
@@ -6,6 +6,9 @@ class Couleur {
   Couleur.rouge() : r = 255, g = 0, b = 0;
   Couleur.vert() : r = 0, g = 255, b = 0;
   Couleur.bleu() : r = 0, g = 0, b = 255;
+  void affichecouleur(){
+    print("R : $r, V : $g, B : $b");
+  }
   }
  
  
