1c1795ed357fd35547b1c2d2da092ad3ffe7736e
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-09 21:27:27 -0500

    methode

--- exercice3.dart
+++ exercice3.dart
@@ -3,6 +3,11 @@ class Compte{
   final String numeroCompte;
   Compte(this.numeroCompte, [double solde = 0.0]) : _solde = solde;
   String get soldeFormate => _solde.toString();
-  
+    void depot(double montant){
+    if (montant > 0){
+    _solde += montant;
+    }
+  }
+
 
 }
