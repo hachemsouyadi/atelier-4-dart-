a636480661fbfd4d2868c75fe92ea45bd1a0ceee
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-09 22:12:00 -0500

    vd

--- exercice4.dart
+++ exercice4.dart
@@ -2,5 +2,13 @@ class Media{
   final String titre;
   Media(this.titre);
   void afficherType(){
-    print("ceci est un media generique.");}
-    }
+    print("ceci est un media generique.");}}
+    class Livre extends Media{
+      String auteur;
+      Livre(String titre,this.auteur) : super(titre);}
+      @override
+      void afficherType(){
+        print("Ceci est un livre");}
+
+
+    
