a2a4d788eba925a8ccdbd6ca77071ed4a6e8e280
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-10 12:17:26 -0500

    Ajoutez une méthode concrète void afficherMessage() qui af

--- exercice5.dart
+++ exercice5.dart
@@ -2,5 +2,16 @@ abstract class Forme{
   abstract double calculerAire();
     void afficherMessage(){
     print("Calcul de l'aire...");
-  }
+  } 
 }
+class Cercle extends Forme {
+  final double rayon;
+  Cercle(this.rayon);
+  @override
+  double calculerAire(){
+    return 3.14*rayon*rayon;
+  }
+
+}
+
+
