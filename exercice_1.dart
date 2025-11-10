80f323d3714df2094c1076c0dd77d4827d1e00f7
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-09 19:32:29 -0500

    ma classe et mon constructeur noomé

---
+++ exercice_1.dart
@@ -0,0 +1,10 @@
+class Couleur {
+  int r;
+  int g;
+  int b;
+  Couleur(this.r, this.g, this.b);
+
+  Couleur.rouge() : r = 255, g = 0, b = 0;
+  Couleur.vert() : r = 0, g = 255, b = 0;
+  Couleur.bleu() : r = 0, g = 0, b = 255; 
+  }
