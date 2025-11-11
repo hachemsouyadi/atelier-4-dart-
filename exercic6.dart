927a4a30a9d28cafacf296591e3d43f43f85c541
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-11 18:50:09 -0500

    Appelez afficherMessage() et calculerAire() pour chaque instance

--- exercic6.dart
+++ exercic6.dart
@@ -1,15 +1,39 @@
-class Connectable{
+abstract class Connectable{
   void connecter(String utilisateur);
   void deconnecter();}
   class ServeurAPI implements Connectable{
   @override
   void connecter(String utilisateur){
-    print("Serveur api : Connexion etablie pour $utilisateur");
+    print("Serveur api:Connexion etablie pour $utilisateur");
   }
   @override
   void deconnecter(){
-    print("serveur api : deconnexion effectue.");
+    print("serveur api:deconnexion effectue");
 
   }
 }
+class Basededonnees implements Connectable{
+  @override
+  void connecter(String utilisateur){
+    print("Basededonnees:Connexion etablie pour $utilisateur");
+  }
+  @override
+  void deconnecter(){
+    print("Basededonnees:deconnexion effectuee.");}
+}
+class ServiceTest{
+  void testerConnexion(Connectable c,String utilisateur){
+    c.connecter(utilisateur);
+    c.deconnecter();}
+}
+void main(){
+  var serveur=ServeurAPI();
+  var base=Basededonnees();
+  var test=ServiceTest();
+  test.testerConnexion(serveur,"Hachem");
+  test.testerConnexion(base,"Hachem");
+}
+
+
+
   
