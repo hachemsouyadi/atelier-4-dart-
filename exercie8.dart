32d852041cebfa61f087a06690b1e52564005350
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-11 19:41:58 -0500

    10

--- exercie8.dart
+++ exercie8.dart
@@ -11,25 +11,27 @@ class Livre{
   print("titre :$titre,auteur :$auteur, Pages :$pages");
 }
 }
-void main() {
-  var l1 = Livre("1850","elmortad");
-  var l2 = Livre("lhmama","morad rabai");
-  l1.afficherInfos();
-  l2.afficherInfos();
-}
+
 class Roman extends Livre{
   String genre;
   Roman(String titre, String auteur, this.genre) : super(titre, auteur);
   @override
   void afficherInfos(){
     print("Titre :$titre, Auteur :$auteur, Genre :$genre, Pages :$pages");
-
-
+}
+}
+void main() {
+  var r1 = Roman("Inferno", "Dan Brown", "Thriller");
+  var r2 = Roman("Les Misérables", "Victor Hugo", "Drame");
+  r1.afficherInfos();
+  r2.afficherInfos();
 
   
+  var l1 = Livre("1850","elmortad");
+  var l2 = Livre("lhmama","morad rabai");
+  l1.afficherInfos();
+  l2.afficherInfos();
 }
 
-  
-  }
 
 
